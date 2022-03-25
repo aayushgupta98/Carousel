f0d80808c40f307646104e1d6b3aeb23cd1c631b
Aayush Gupta <63859620+aayushgupta98@users.noreply.github.com>
2022-03-25 14:42:39 +0530

    first commit

---
+++ carousel.js
@@ -0,0 +1,47 @@
+let picId = 0;
+let totalPics = document.getElementsByClassName('carousel-image');
+let totalPicsLength = totalPics.length;
+
+document.getElementById('previous').addEventListener("click", function() {
+    previousPic();
+  });
+document.getElementById('next').addEventListener("click", function() {
+    nextPic();
+  });
+
+let prevBtn = document.getElementById('previous');
+let nextBtn = document.getElementById('next');
+if(picId == 0){
+    prevBtn.setAttribute("disabled","disabled");
+}
+
+function nextPic(){
+    let currentPic = totalPics[picId];
+    currentPic.classList.remove("visible");
+    currentPic.classList.add("hidden");
+    picId++;
+    if(picId == totalPics.length - 1){
+        nextBtn.setAttribute("disabled","disabled");
+    }
+    if(picId != 0){
+        prevBtn.removeAttribute("disabled");
+    }
+    let nextPic = totalPics[picId];
+    nextPic.classList.remove("hidden");
+    nextPic.classList.add("visible");
+}
+function previousPic(){
+    let currentPic = totalPics[picId];
+    currentPic.classList.remove("visible");
+    currentPic.classList.add("hidden");
+    picId--;
+    if(picId == 0){
+        prevBtn.setAttribute("disabled","disabled");
+    }
+    if(picId != totalPics.length - 1){
+        nextBtn.removeAttribute("disabled","disabled");
+    }
+    let nextPic = totalPics[picId];
+    nextPic.classList.remove("hidden");
+    nextPic.classList.add("visible");
+}
